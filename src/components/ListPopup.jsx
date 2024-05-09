1ce02d0e391638bd3c0a8cd0fe173d456776f293
hamidu <hamidusodiq2@gmail.com>
2024-05-09 14:40:45 +0100

    adjust appearance of listPopup component

--- src/components/ListPopup.jsx
+++ src/components/ListPopup.jsx
@@ -3,9 +3,9 @@ import { faFloppyDisk, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const ListPopup = ({ items }) => {
   return (
-    <div className="px-4 py-4 absolute right-2 bg-white shadow-md w-72 z-50 flex flex-col gap-5 max-h-60 overflow-auto">
+    <div className="px-4 py-4 absolute right-2 mt-2 rounded-lg bg-white box-shadow w-72 z-50 flex flex-col gap-5 max-h-60 overflow-auto font-medium text-secondary-80">
       {items.map((item) => (
-        <li className="flex justify-between items-center ">
+        <li className="flex justify-between items-center text-sm mb-2">
           <span>{item}</span>
           <FontAwesomeIcon
             icon={faPlus}
